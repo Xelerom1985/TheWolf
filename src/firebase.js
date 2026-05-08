1757b3264a3fa357bc6467676a3bb6ff4aee2ced
Fernando Adrian Flores <fflsonct@pluspetrol.net>
2026-05-08 00:50:06 -0300

    Add Firebase config and complete The Wolf Barberia PWA

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_placeholder",
+  apiKey: "AIzaSyDqffW0BBdDdPS0C4cGAh3cvdlkQ-xSZ38",
   authDomain: "the-wolf-a94f8.firebaseapp.com",
   databaseURL: "https://the-wolf-a94f8-default-rtdb.firebaseio.com",
   projectId: "the-wolf-a94f8",
-  storageBucket: "the-wolf-a94f8.appspot.com",
-  messagingSenderId: "placeholder",
-  appId: "placeholder"
+  storageBucket: "the-wolf-a94f8.firebasestorage.app",
+  messagingSenderId: "350304651653",
+  appId: "1:350304651653:web:fee82d1538e22660925cec",
 }
 
 const app = initializeApp(firebaseConfig)
